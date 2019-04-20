bc4549c4ccd37949bd79997f958925e7261d845b
ilpersi <lpersichetti@gmail.com>
2019-04-20 11:58:04 +0200

    Removal of old autorevive.

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -18,7 +18,6 @@ public class Settings {
 	boolean restartAfterAdOfferTimeout = true; // if true, then bot will automatically restart itself if it hasn't claimed any ad offer in a time longer than defined. This is needed because ads don't appear anymore if Chrome doesn't get restarted.
 	boolean debugDetectionTimes = false; // if true, then each time a cue detection from game screenshot will be attempted, a time taken will be displayed together with a name of the cue
 	boolean hideWindowOnRestart = false; // if true, game window will be hidden upon driver (re)start
-	boolean experimentalAutoRevive = true; // if true, the new EXPERIMENTAL AutoRevive system will be used
 	private boolean resetTimersOnBattleEnd = true; // if true, readout timers will get reset once dungeon is cleared (or pvp or gvg or any other type of battle)
 	int reconnectTimer = 60;
 
@@ -483,7 +482,6 @@ public class Settings {
 		restartAfterAdOfferTimeout = map.getOrDefault("restartAfterAdOfferTimeout", restartAfterAdOfferTimeout ? "1" : "0").equals("1");
 		debugDetectionTimes = map.getOrDefault("debugDetectionTimes", debugDetectionTimes ? "1" : "0").equals("1");
 		hideWindowOnRestart = map.getOrDefault("hideWindowOnRestart", hideWindowOnRestart ? "1" : "0").equals("1");
-		experimentalAutoRevive = map.getOrDefault("experimentalAutoRevive", experimentalAutoRevive ? "1" : "0").equals("1");
 		resetTimersOnBattleEnd = map.getOrDefault("resetTimersOnBattleEnd", resetTimersOnBattleEnd ? "1" : "0").equals("1");
 		autoStartChromeDriver = map.getOrDefault("autoStartChromeDriver", autoStartChromeDriver ? "1" : "0").equals("1");
 		reconnectTimer = Integer.parseInt(map.getOrDefault("reconnectTimer", ""+reconnectTimer));
@@ -595,11 +593,14 @@ public class Settings {
 		if (map.getOrDefault("expeditionDifficulty", null) != null) {
 			BHBot.log("Deprecated setting detected: expeditionDifficulty. Use the new expedition format instead.");
 		}
+		if (map.getOrDefault("experimentalAutoRevive", null) != null) {
+			BHBot.log("Deprecated setting detected: experimentalAutoRevive. Old revive system is no longer available.");
+		}
 	}
 
 	private void sanitizeSetting(Map<String, String> map) {
 		// we check and sanitize expeditions values
-		String expeditions = map.get("expeditions");
+		String expeditions = map.getOrDefault("expeditions", "");
 		if (expeditions.contains("h") || expeditions.contains("i")) {
 			BHBot.log("WARNING: invalid format detected for expeditions settings '" + expeditions + "': " +
 					"a standard value of 'p1 100 100' will be used" );
